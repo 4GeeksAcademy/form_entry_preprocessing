3264c40da8e2f5113f91e43eb813876f8ffaf8f2
Daniela Alvarez Zegarra <daniela.alvarez04@gmail.com>
2022-09-03 11:00:46 -0500

    improvement in cleaning process

---
+++ src/build_features.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import numpy as np
+
+#PREPROCESSING FUNCTIONS
+
+def change_to_datetime(df,column):
+    df[column] = df[column].apply(pd.to_datetime).copy()
+    return df
+
+def change_format(df,column):
+    df[column] = df[column].dt.strftime("%Y-%m-%d %H:%M:%S")
+    return df
+
+def combine_columns(df, column1, column2, new_column):
+    df[new_column] = df[column1].fillna('') + str(' ') + df[column2].fillna('')   
+    return df
+
+def drop_null_columns(df):
+    df.dropna(axis=1, how='all', inplace=True)  
+    return df
+
+def drop_test_rows(df):
+    df = df[df["email"].str.contains("@4geeks") == False]
+    return df
+
+def remove_duplicates(df):
+    print('shape before combining duplicates: ', df.shape)
+    df = df.replace("Nan", np.nan).copy()
+    df = df.sort_values("created_at").copy()
+    df = df.groupby("email").first().reset_index()
+    print('shape after combining duplicates: ', df.shape)
+    return df
+    
+def clean_course(df,column):
+    df[column] = df[column].replace(['full-stack-ft', 'full_stack', 'full-stack,software-engineering',
+                                    'coding-introduction','outcomes'], 'full-stack')
+    df[column] = df[column].replace(['machine-learning', 'machine-learning-enginnering'], 
+                                    'machine-learning-engineering')
+    print(df[column].value_counts())
+    return df
+ 
+def clean_location(df,column):
+    df[column] = df[column].replace(['maracaibo'], 'maracaibo-venezuela')
+    df[column] = df[column].replace(['los-cortijos-caracas'], 'caracas-venezuela') 
+    print(df[column].value_counts())
+    return df
+  
+    
+def clean_language(df,column):
+    df[column] = df[column].replace('us', 'en')
+    print(df[column].value_counts())
+    return df
+    
+    
+def clean_utm_source(df,column):
+    df[column] = df[column].replace('LInkedin', 'linkedin')
+    df[column] = df[column].replace('CourseReport', 'coursereport')
+    df[column] = df[column].replace(['landingjobs?utm_medium=machine-learning-engineering', 
+                                    'landingjobs?utm_medium=full-stack', 'landingjobs?utm_medium=RRSS'],
+                                    'landingjobs')
+    df[column] = df[column].replace('google_ads', 'google')
+    df[column] = df[column].replace(['Business Manager IG', 'Instagram_Feed', 'ig', 'Instagram_Stories'], 'instagram')
+    df[column] = df[column].replace(['Facebook','Facebook ads','Facebook_Marketplace','Facebook_Mobile_Feed',
+                                    'facebook_instagram','fb','an','facebook_awareness','Facebook_Stories',
+                                    'Facebook_Desktop_Feed'], 'facebook')
+    df[column] = df[column].replace('4geeks', 'ticjob')
+    print(df[column].value_counts())
+    return df
+
+
+def clean_utm_medium(df,column):
+    df[column] = df[column].replace(['schoolpage','coursereportschoolpage', 'schoolpage?utm_source=careerkarma',
+                                    'Blog','affiliate_email','rrss','inscripcion','event'], 'referral')
+    df[column] = df[column].replace(['ppc','FB paid','Facebook_Mobile_Feed','Instagram_Stories','Instagram_Feed'],
+                                    'cpc')
+    df[column] = np.where((df['utm_source'] == 'linkedin') & (df[column] == 'social') , 
+                              'cpc', df[column])
+
+    df[column] = np.where((df['utm_source'] == 'linkedin') & (df['column] == 'Inmail') , 
+                              'cpc', df[column])
+    print(df[column].value_counts())
+    return df      
+    
+
+def assign_lead_type(df,column1,column2):
+    df.loc[df[column1] == 'request_more_info', column2] = 'SOFT'
+    df.loc[df[column1] == 'website-lead', column2] = 'STRONG'
+    df.loc[df[column1] == 'newsletter', column2] = 'DISCOVERY'
+    df.loc[df[column1] == 'contact-us', column2] = 'SOFT'
+    df.loc[df[column1] == 'utec-uruguay', column2] = 'STRONG'
+    df.loc[df[column1] == 'jobboard-lead', column2] = 'STRONG'
+    df.loc[df[column1] == 'hiring-partner', column2] = 'OTHER'
+    df.loc[df[column1] == 'download_outcome', column2] = 'DISCOVERY'
+    df.loc[df[column1] == 'website-lead,blacks-in-technology', column2] = 'STRONG'
+    df.loc[df[column1] == 'request_downloadable', column2] = 'DISCOVERY'
+    print(df[column2].value_counts())
+    return df
+
+def assign_with_conditions(df):
+    df['utm_medium'] = np.where((df['utm_source'] == 'Facebook ads') | 
+                              (df['utm_source'] == 'Facebook_Marketplace') | 
+                              (df['utm_source'] == 'Facebook_Mobile_Feed') |
+                              (df['utm_source'] == 'facebook_awareness') |
+                              (df['utm_source'] == 'Facebook_Stories') |
+                              (df['utm_source'] == 'Facebook_Desktop_Feed') |
+                              (df['utm_source'] == 'Business Manager IG') |
+                              (df['utm_source'] == 'Instagram_Feed') |
+                              (df['utm_source'] == 'Instagram_Stories'), 
+                              'cpc', df['utm_medium'])
+
+    df['utm_source'] = np.where((df['utm_medium'] == 'Instagram_Stories') |
+                              (df['utm_medium'] == 'Instagram_Feed'),
+                              'instagram', df['utm_source'])
+
+    df['utm_source'] = np.where((df['utm_medium'] == 'Facebook_Mobile_Feed'),
+                              'facebook', df['utm_source'])
+       
+    print('Assignation with conditions ok')
+    return df
+  
+#Visualization features
+    
+def countplot_features(df, feature):
+    fig = plt.figure(figsize=(10,6))
+    ax = sns.countplot(x=df[feature], order=df[feature].value_counts(ascending=False).index);
+
+    abs_values = df[feature].value_counts(ascending=False).values
+    ax.bar_label(container=ax.containers[0], labels=abs_values)
+    plt.xticks(rotation=30, ha='right')
+    #plt.show()
+    
+def countplot_targetvsfeature(df,feature,target):
+    fig = plt.figure(figsize=(10,6))
+    ax = sns.countplot(x=df[feature], hue=target, order=df[feature].value_counts(ascending=False).index);
+
+    abs_values = df[feature].value_counts(ascending=False).values
+    ax.bar_label(container=ax.containers[0], labels=abs_values)
+    plt.xticks(rotation=30, ha='right')
+    #plt.show()
---
+++ src/cleaning_process.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+
+import build_features as features
+
+from constants import TO_DROP
+
+#Loading the data
+data = pd.read_csv('../data/raw/All_Form_Entries.csv', dtype={'utm_content': 'object', 
+                                                              'current_download': 'object', 
+                                                              'state': 'object', 'lead_type': 'object',
+                                                              'lead_generation_app':'object'})
+
+
+
+#Changing type of columns 
+features.change_to_datetime(data,'created_at')
+features.change_to_datetime(data,'updated_at')
+features.change_to_datetime(data,'won_at')
+
+#Changing format
+features.change_format(data,'created_at')
+features.change_format(data,'updated_at')
+features.change_format(data,'won_at')
+
+#Format change also changed the column type to object, so we need to convert it to datetime again 
+features.change_to_datetime(data,'created_at')
+features.change_to_datetime(data,'updated_at')
+features.change_to_datetime(data,'won_at')
+
+#creating new columns
+data['year-month'] = data['created_at'].dt.strftime('%Y-%m')
+data['created_time'] = data['created_at'].dt.strftime('%H:%M:%S')
+data['has_gclid'] = np.where(data['gclid'].isnull(), '0', '1')
+data['days_to_convert'] = (data['won_at'] - data['created_at']).dt.days.abs()
+
+
+#Combine first and last name ignoring nulls
+features.combine_columns(data, 'first_name', 'last_name', 'fullname')
+
+# shift column 'Fullname' to third position
+fourth_column = data.pop('fullname')
+data.insert(3, 'fullname', fourth_column)
+
+#drop null columns
+features.drop_null_columns(data)
+
+
+#drop irrelevant columns
+data.drop(TO_DROP, axis=1, inplace=True)
+
+#drop testing rows
+features.drop_test_rows(data)
+
+#dropping two other test rows identified
+data.drop(data[data['utm_source'] == 'test_s'].index, inplace = True)
+data.drop(data[data['utm_source'] == 'fintech'].index, inplace = True)
+
+print('Number of columns after dropping columns: ', data.shape)
+
+#remove duplicated rows by combining them considering the first creation date
+data = features.remove_duplicates(data)
+
+#Clean course column
+data = features.clean_course(data,'course')
+
+#Clean location column
+data = features.clean_location(data,'location')
+
+#clean language column
+data = features.clean_language(data,'language')
+
+#clean utm_source
+
+data = features.assign_with_conditions(data)
+
+data = features.clean_utm_source(data,'utm_source')
+
+#clean utm_medium
+data = features.clean_utm_medium(data,'utm_medium')
+
+# Assign values
+data = features.assign_lead_type(data, 'tags','lead_type')
+
+print(data.shape)
